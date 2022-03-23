db622a2ae52b0300b4a285d4aa6004dc9e029de3
Crowdbotics <team@crowdbotics.com>
2022-03-23 23:39:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sonic_game_34403",
+  url: "https://sonic_game_34403.botics.co",
+  api: "https://sonic_game_34403.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
